8a9f651b826e1ef8f587ab95544b653bbee1e451
Aleksander Rokita <kelor2@o2.pl>
2025-04-05 13:03:24 +0200

    Meet flake8

--- app/main.py
+++ app/main.py
@@ -1,30 +1,33 @@
+from typing import Any
+
+
 class Dictionary:
-    def __init__(self):
+    def __init__(self) -> None:
         self._length = 0
         self._table_size = 8
-        self._threshold_factor = 2/3
+        self._threshold_factor = 2 / 3
         self._table: list = [None] * self._table_size
 
     @property
-    def _threshold(self):
+    def _threshold(self) -> int:
         return int(self._table_size * self._threshold_factor)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self._length
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self._table)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Any, value: Any) -> None:
         if self._length + 1 >= self._threshold:
             self._resize()
         self._insert(key, value)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any) -> Any:
         index = self._find_key(key)
         return self._table[index][1]
 
-    def _resize(self):
+    def _resize(self) -> None:
         old_table = self._table
         self._table_size *= 2
         self._table: list = [None] * self._table_size
@@ -34,13 +37,13 @@ class Dictionary:
             if entry is not None:
                 self._insert(*entry)
 
-    def _insert(self, key, value):
+    def _insert(self, key: Any, value: Any) -> None:
         index = self._probe(key)
         if self._table[index] is None:
             self._length += 1
         self._table[index] = key, value
 
-    def _find_key(self, key):
+    def _find_key(self, key: Any) -> int:
         start_index = hash(key) % self._table_size
         for i in range(self._table_size):
             index = (start_index + i) % self._table_size
@@ -51,10 +54,12 @@ class Dictionary:
                 return index
         raise KeyError
 
-    def _probe(self, key):
+    def _probe(self, key: Any) -> int:
         start_index = hash(key) % self._table_size
         for i in range(self._table_size):
             index = (start_index + i) % self._table_size
-            if self._table[index] is None or (self._table[index][0] == key and hash(self._table[index][0]) == hash(key)):
+            if (self._table[index] is None
+                    or (self._table[index][0] == key
+                        and hash(self._table[index][0]) == hash(key))):
                 return index
         raise RuntimeError
