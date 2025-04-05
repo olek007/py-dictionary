ec80d3a4cfdfc7e8fc5444d4846fb30bab1fe99d
Aleksander Rokita <kelor2@o2.pl>
2025-04-05 13:00:43 +0200

    Solution + refactor

--- app/main.py
+++ app/main.py
@@ -1,2 +1,60 @@
 class Dictionary:
-    pass
+    def __init__(self):
+        self._length = 0
+        self._table_size = 8
+        self._threshold_factor = 2/3
+        self._table: list = [None] * self._table_size
+
+    @property
+    def _threshold(self):
+        return int(self._table_size * self._threshold_factor)
+
+    def __len__(self):
+        return self._length
+
+    def __repr__(self):
+        return str(self._table)
+
+    def __setitem__(self, key, value):
+        if self._length + 1 >= self._threshold:
+            self._resize()
+        self._insert(key, value)
+
+    def __getitem__(self, key):
+        index = self._find_key(key)
+        return self._table[index][1]
+
+    def _resize(self):
+        old_table = self._table
+        self._table_size *= 2
+        self._table: list = [None] * self._table_size
+        self._length = 0
+
+        for entry in old_table:
+            if entry is not None:
+                self._insert(*entry)
+
+    def _insert(self, key, value):
+        index = self._probe(key)
+        if self._table[index] is None:
+            self._length += 1
+        self._table[index] = key, value
+
+    def _find_key(self, key):
+        start_index = hash(key) % self._table_size
+        for i in range(self._table_size):
+            index = (start_index + i) % self._table_size
+            entry = self._table[index]
+            if entry is None:
+                continue
+            if entry[0] == key and hash(entry[0]) == hash(key):
+                return index
+        raise KeyError
+
+    def _probe(self, key):
+        start_index = hash(key) % self._table_size
+        for i in range(self._table_size):
+            index = (start_index + i) % self._table_size
+            if self._table[index] is None or (self._table[index][0] == key and hash(self._table[index][0]) == hash(key)):
+                return index
+        raise RuntimeError
